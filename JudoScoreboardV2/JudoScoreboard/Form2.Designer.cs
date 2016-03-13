dcbff4164b9de11ceae9e79fc660da00d9f0aa50
Rob van Berkel <rob.vanberkel@planet.nl>
2016-03-13 14:27:58 +0100

    First code
    Committed on 13/03

---
+++ JudoScoreboardV2/JudoScoreboard/Form2.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace JudoScoreboard
+{
+    partial class publicScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(publicScreen));
+            this.lblYukoWit = new System.Windows.Forms.Label();
+            this.lblYukoRood = new System.Windows.Forms.Label();
+            this.lblWazariRood = new System.Windows.Forms.Label();
+            this.lblIpponRood = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblShidoRood = new System.Windows.Forms.Label();
+            this.lblWazariWit = new System.Windows.Forms.Label();
+            this.lblIpponWit = new System.Windows.Forms.Label();
+            this.lblShidoWit = new System.Windows.Forms.Label();
+            this.lblHoldingWit = new System.Windows.Forms.Label();
+            this.lblHoldingRood = new System.Windows.Forms.Label();
+            this.lblConfirmRood1 = new System.Windows.Forms.Label();
+            this.lblConfirmRood2 = new System.Windows.Forms.Label();
+            this.lblConfirmRood3 = new System.Windows.Forms.Label();
+            this.lblConfirmWit1 = new System.Windows.Forms.Label();
+            this.lblConfirmWit2 = new System.Windows.Forms.Label();
+            this.lblConfirmWit3 = new System.Windows.Forms.Label();
+            this.lblShidoTextWit = new System.Windows.Forms.Label();
+            this.lblShidoTextRood = new System.Windows.Forms.Label();
+            this.lblYukoTextWit = new System.Windows.Forms.Label();
+            this.lblWazariTextWit = new System.Windows.Forms.Label();
+            this.lblIpponTextWit = new System.Windows.Forms.Label();
+            this.lblYukoTextRood = new System.Windows.Forms.Label();
+            this.lblWazariTextRood = new System.Windows.Forms.Label();
+            this.lblIpponTextRood = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblYukoWit
+            // 
+            this.lblYukoWit.AutoSize = true;
+            this.lblYukoWit.Location = new System.Drawing.Point(263, 111);
+            this.lblYukoWit.Name = "lblYukoWit";
+            this.lblYukoWit.Size = new System.Drawing.Size(13, 13);
+            this.lblYukoWit.TabIndex = 0;
+            this.lblYukoWit.Text = "0";
+            // 
+            // lblYukoRood
+            // 
+            this.lblYukoRood.AutoSize = true;
+            this.lblYukoRood.Location = new System.Drawing.Point(1103, 111);
+            this.lblYukoRood.Name = "lblYukoRood";
+            this.lblYukoRood.Size = new System.Drawing.Size(13, 13);
+            this.lblYukoRood.TabIndex = 1;
+            this.lblYukoRood.Text = "0";
+            // 
+            // lblWazariRood
+            // 
+            this.lblWazariRood.AutoSize = true;
+            this.lblWazariRood.Location = new System.Drawing.Point(1287, 111);
+            this.lblWazariRood.Name = "lblWazariRood";
+            this.lblWazariRood.Size = new System.Drawing.Size(13, 13);
+            this.lblWazariRood.TabIndex = 2;
+            this.lblWazariRood.Text = "0";
+            // 
+            // lblIpponRood
+            // 
+            this.lblIpponRood.AutoSize = true;
+            this.lblIpponRood.Location = new System.Drawing.Point(1477, 111);
+            this.lblIpponRood.Name = "lblIpponRood";
+            this.lblIpponRood.Size = new System.Drawing.Size(13, 13);
+            this.lblIpponRood.TabIndex = 3;
+            this.lblIpponRood.Text = "0";
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Location = new System.Drawing.Point(936, 676);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(19, 13);
+            this.lblSec.TabIndex = 9;
+            this.lblSec.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(862, 676);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = ":";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(681, 676);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(19, 13);
+            this.lblMin.TabIndex = 7;
+            this.lblMin.Text = "00";
+            // 
+            // lblShidoRood
+            // 
+            this.lblShidoRood.AutoSize = true;
+            this.lblShidoRood.Location = new System.Drawing.Point(1477, 513);
+            this.lblShidoRood.Name = "lblShidoRood";
+            this.lblShidoRood.Size = new System.Drawing.Size(13, 13);
+            this.lblShidoRood.TabIndex = 10;
+            this.lblShidoRood.Text = "0";
+            // 
+            // lblWazariWit
+            // 
+            this.lblWazariWit.AutoSize = true;
+            this.lblWazariWit.Location = new System.Drawing.Point(465, 111);
+            this.lblWazariWit.Name = "lblWazariWit";
+            this.lblWazariWit.Size = new System.Drawing.Size(13, 13);
+            this.lblWazariWit.TabIndex = 11;
+            this.lblWazariWit.Text = "0";
+            // 
+            // lblIpponWit
+            // 
+            this.lblIpponWit.AutoSize = true;
+            this.lblIpponWit.Location = new System.Drawing.Point(666, 111);
+            this.lblIpponWit.Name = "lblIpponWit";
+            this.lblIpponWit.Size = new System.Drawing.Size(13, 13);
+            this.lblIpponWit.TabIndex = 12;
+            this.lblIpponWit.Text = "0";
+            // 
+            // lblShidoWit
+            // 
+            this.lblShidoWit.AutoSize = true;
+            this.lblShidoWit.Location = new System.Drawing.Point(263, 513);
+            this.lblShidoWit.Name = "lblShidoWit";
+            this.lblShidoWit.Size = new System.Drawing.Size(13, 13);
+            this.lblShidoWit.TabIndex = 13;
+            this.lblShidoWit.Text = "0";
+            // 
+            // lblHoldingWit
+            // 
+            this.lblHoldingWit.AutoSize = true;
+            this.lblHoldingWit.Location = new System.Drawing.Point(681, 513);
+            this.lblHoldingWit.Name = "lblHoldingWit";
+            this.lblHoldingWit.Size = new System.Drawing.Size(13, 13);
+            this.lblHoldingWit.TabIndex = 14;
+            this.lblHoldingWit.Text = "0";
+            // 
+            // lblHoldingRood
+            // 
+            this.lblHoldingRood.AutoSize = true;
+            this.lblHoldingRood.Location = new System.Drawing.Point(1024, 513);
+            this.lblHoldingRood.Name = "lblHoldingRood";
+            this.lblHoldingRood.Size = new System.Drawing.Size(13, 13);
+            this.lblHoldingRood.TabIndex = 15;
+            this.lblHoldingRood.Text = "0";
+            // 
+            // lblConfirmRood1
+            // 
+            this.lblConfirmRood1.AutoSize = true;
+            this.lblConfirmRood1.Location = new System.Drawing.Point(1236, 767);
+            this.lblConfirmRood1.Name = "lblConfirmRood1";
+            this.lblConfirmRood1.Size = new System.Drawing.Size(13, 13);
+            this.lblConfirmRood1.TabIndex = 16;
+            this.lblConfirmRood1.Text = "0";
+            // 
+            // lblConfirmRood2
+            // 
+            this.lblConfirmRood2.AutoSize = true;
+            this.lblConfirmRood2.Location = new System.Drawing.Point(1392, 767);
+            this.lblConfirmRood2.Name = "lblConfirmRood2";
+            this.lblConfirmRood2.Size = new System.Drawing.Size(13, 13);
+            this.lblConfirmRood2.TabIndex = 17;
+            this.lblConfirmRood2.Text = "0";
+            // 
+            // lblConfirmRood3
+            // 
+            this.lblConfirmRood3.AutoSize = true;
+            this.lblConfirmRood3.Location = new System.Drawing.Point(1536, 767);
+            this.lblConfirmRood3.Name = "lblConfirmRood3";
+            this.lblConfirmRood3.Size = new System.Drawing.Size(13, 13);
+            this.lblConfirmRood3.TabIndex = 18;
+            this.lblConfirmRood3.Text = "0";
+            // 
+            // lblConfirmWit1
+            // 
+            this.lblConfirmWit1.AutoSize = true;
+            this.lblConfirmWit1.Location = new System.Drawing.Point(161, 767);
+            this.lblConfirmWit1.Name = "lblConfirmWit1";
+            this.lblConfirmWit1.Size = new System.Drawing.Size(13, 13);
+            this.lblConfirmWit1.TabIndex = 19;
+            this.lblConfirmWit1.Text = "0";
+            // 
+            // lblConfirmWit2
+            // 
+            this.lblConfirmWit2.AutoSize = true;
+            this.lblConfirmWit2.Location = new System.Drawing.Point(277, 767);
+            this.lblConfirmWit2.Name = "lblConfirmWit2";
+            this.lblConfirmWit2.Size = new System.Drawing.Size(13, 13);
+            this.lblConfirmWit2.TabIndex = 20;
+            this.lblConfirmWit2.Text = "0";
+            // 
+            // lblConfirmWit3
+            // 
+            this.lblConfirmWit3.AutoSize = true;
+            this.lblConfirmWit3.Location = new System.Drawing.Point(423, 767);
+            this.lblConfirmWit3.Name = "lblConfirmWit3";
+            this.lblConfirmWit3.Size = new System.Drawing.Size(13, 13);
+            this.lblConfirmWit3.TabIndex = 21;
+            this.lblConfirmWit3.Text = "0";
+            // 
+            // lblShidoTextWit
+            // 
+            this.lblShidoTextWit.AutoSize = true;
+            this.lblShidoTextWit.Location = new System.Drawing.Point(263, 437);
+            this.lblShidoTextWit.Name = "lblShidoTextWit";
+            this.lblShidoTextWit.Size = new System.Drawing.Size(34, 13);
+            this.lblShidoTextWit.TabIndex = 44;
+            this.lblShidoTextWit.Text = "Shido";
+            // 
+            // lblShidoTextRood
+            // 
+            this.lblShidoTextRood.AutoSize = true;
+            this.lblShidoTextRood.Location = new System.Drawing.Point(1477, 437);
+            this.lblShidoTextRood.Name = "lblShidoTextRood";
+            this.lblShidoTextRood.Size = new System.Drawing.Size(34, 13);
+            this.lblShidoTextRood.TabIndex = 45;
+            this.lblShidoTextRood.Text = "Shido";
+            // 
+            // lblYukoTextWit
+            // 
+            this.lblYukoTextWit.AutoSize = true;
+            this.lblYukoTextWit.Location = new System.Drawing.Point(263, 29);
+            this.lblYukoTextWit.Name = "lblYukoTextWit";
+            this.lblYukoTextWit.Size = new System.Drawing.Size(32, 13);
+            this.lblYukoTextWit.TabIndex = 46;
+            this.lblYukoTextWit.Text = "Yuko";
+            // 
+            // lblWazariTextWit
+            // 
+            this.lblWazariTextWit.AutoSize = true;
+            this.lblWazariTextWit.Location = new System.Drawing.Point(456, 29);
+            this.lblWazariTextWit.Name = "lblWazariTextWit";
+            this.lblWazariTextWit.Size = new System.Drawing.Size(40, 13);
+            this.lblWazariTextWit.TabIndex = 47;
+            this.lblWazariTextWit.Text = "Wazari";
+            // 
+            // lblIpponTextWit
+            // 
+            this.lblIpponTextWit.AutoSize = true;
+            this.lblIpponTextWit.Location = new System.Drawing.Point(666, 29);
+            this.lblIpponTextWit.Name = "lblIpponTextWit";
+            this.lblIpponTextWit.Size = new System.Drawing.Size(34, 13);
+            this.lblIpponTextWit.TabIndex = 48;
+            this.lblIpponTextWit.Text = "Ippon";
+            // 
+            // lblYukoTextRood
+            // 
+            this.lblYukoTextRood.AutoSize = true;
+            this.lblYukoTextRood.Location = new System.Drawing.Point(1103, 29);
+            this.lblYukoTextRood.Name = "lblYukoTextRood";
+            this.lblYukoTextRood.Size = new System.Drawing.Size(32, 13);
+            this.lblYukoTextRood.TabIndex = 49;
+            this.lblYukoTextRood.Text = "Yuko";
+            // 
+            // lblWazariTextRood
+            // 
+            this.lblWazariTextRood.AutoSize = true;
+            this.lblWazariTextRood.Location = new System.Drawing.Point(1278, 29);
+            this.lblWazariTextRood.Name = "lblWazariTextRood";
+            this.lblWazariTextRood.Size = new System.Drawing.Size(40, 13);
+            this.lblWazariTextRood.TabIndex = 50;
+            this.lblWazariTextRood.Text = "Wazari";
+            // 
+            // lblIpponTextRood
+            // 
+            this.lblIpponTextRood.AutoSize = true;
+            this.lblIpponTextRood.Location = new System.Drawing.Point(1477, 29);
+            this.lblIpponTextRood.Name = "lblIpponTextRood";
+            this.lblIpponTextRood.Size = new System.Drawing.Size(34, 13);
+            this.lblIpponTextRood.TabIndex = 51;
+            this.lblIpponTextRood.Text = "Ippon";
+            // 
+            // publicScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1582, 811);
+            this.Controls.Add(this.lblIpponTextRood);
+            this.Controls.Add(this.lblWazariTextRood);
+            this.Controls.Add(this.lblYukoTextRood);
+            this.Controls.Add(this.lblIpponTextWit);
+            this.Controls.Add(this.lblWazariTextWit);
+            this.Controls.Add(this.lblYukoTextWit);
+            this.Controls.Add(this.lblShidoTextRood);
+            this.Controls.Add(this.lblShidoTextWit);
+            this.Controls.Add(this.lblConfirmWit3);
+            this.Controls.Add(this.lblConfirmWit2);
+            this.Controls.Add(this.lblConfirmWit1);
+            this.Controls.Add(this.lblConfirmRood3);
+            this.Controls.Add(this.lblConfirmRood2);
+            this.Controls.Add(this.lblConfirmRood1);
+            this.Controls.Add(this.lblHoldingRood);
+            this.Controls.Add(this.lblHoldingWit);
+            this.Controls.Add(this.lblShidoWit);
+            this.Controls.Add(this.lblIpponWit);
+            this.Controls.Add(this.lblWazariWit);
+            this.Controls.Add(this.lblShidoRood);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblIpponRood);
+            this.Controls.Add(this.lblWazariRood);
+            this.Controls.Add(this.lblYukoRood);
+            this.Controls.Add(this.lblYukoWit);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "publicScreen";
+            this.Text = "Judoscoreboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label lblYukoWit;
+        public System.Windows.Forms.Label lblYukoRood;
+        public System.Windows.Forms.Label lblWazariRood;
+        public System.Windows.Forms.Label lblIpponRood;
+        public System.Windows.Forms.Label lblSec;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label lblMin;
+        public System.Windows.Forms.Label lblShidoRood;
+        public System.Windows.Forms.Label lblWazariWit;
+        public System.Windows.Forms.Label lblIpponWit;
+        public System.Windows.Forms.Label lblShidoWit;
+        public System.Windows.Forms.Label lblHoldingWit;
+        public System.Windows.Forms.Label lblHoldingRood;
+        public System.Windows.Forms.Label lblConfirmRood1;
+        public System.Windows.Forms.Label lblConfirmRood2;
+        public System.Windows.Forms.Label lblConfirmRood3;
+        public System.Windows.Forms.Label lblConfirmWit1;
+        public System.Windows.Forms.Label lblConfirmWit2;
+        public System.Windows.Forms.Label lblConfirmWit3;
+        public System.Windows.Forms.Label lblShidoTextWit;
+        public System.Windows.Forms.Label lblShidoTextRood;
+        public System.Windows.Forms.Label lblYukoTextWit;
+        public System.Windows.Forms.Label lblWazariTextWit;
+        public System.Windows.Forms.Label lblIpponTextWit;
+        public System.Windows.Forms.Label lblYukoTextRood;
+        public System.Windows.Forms.Label lblWazariTextRood;
+        public System.Windows.Forms.Label lblIpponTextRood;
+    }
+}
